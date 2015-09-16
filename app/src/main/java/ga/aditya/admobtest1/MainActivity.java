3bae4f0ce7fb638e62f193d416948a82a52f1ef9
Aditya Naik <adityanaik@outlook.com>
2015-09-16 21:13:59 +0530

    Add a button in MainActivity to show BannerActivity

--- app/src/main/java/ga/aditya/admobtest1/MainActivity.java
+++ app/src/main/java/ga/aditya/admobtest1/MainActivity.java
@@ -1,9 +1,12 @@
 package ga.aditya.admobtest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnShowBannerAd = (Button) findViewById(R.id.button_show_bannerAd);
+        btnShowBannerAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showBannerAd = new Intent(MainActivity.this, BannerActivity.class);
+                startActivity(showBannerAd);
+            }
+        });
     }
 
     @Override
