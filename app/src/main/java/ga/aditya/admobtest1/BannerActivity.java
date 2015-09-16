9f290bdd039ca656715095a648524602ec8b3f6d
Aditya Naik <adityanaik@outlook.com>
2015-09-16 19:52:24 +0530

    Add BannerActivity with a test unit id

---
+++ app/src/main/java/ga/aditya/admobtest1/BannerActivity.java
@@ -0,0 +1,24 @@
+package ga.aditya.admobtest1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Shows a banner ad.
+ */
+public class BannerActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
